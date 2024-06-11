7393fb10facd4d20516c531bcded9ad7de7dcfe5
SherwinD <sirking1991@gmail.com>
2024-06-11 22:12:13 +0800

    minor refactor

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->check()) {
-        return redirect('/dashboard');
-    }
-
-    return view('welcome');
+    return auth()->check() ?
+        redirect('/dashboard') :
+        view('welcome');
 });
 
 Route::middleware([
